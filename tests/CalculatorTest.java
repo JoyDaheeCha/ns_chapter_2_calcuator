ab4e363f82e8cee7c2d3cc6ff659c691435a7605
JoyDaheeCha <daheecha0612@gmail.com>
2021-02-15 23:17:56 +0900

    Add Junit Library for Test

---
+++ tests/CalculatorTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @org.junit.Test
+    public void add() {
+    }
+
+    @org.junit.Test
+    public void subtract() {
+    }
+
+    @org.junit.Test
+    public void multiply() {
+    }
+
+    @org.junit.Test
+    public void divide() {
+    }
+}
